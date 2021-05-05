e8f4b02ac880b0a8b1b1ba5cca6abea277d5310d
Emmanuel Andre <Emmanuel_Andre@dell.com>
2021-05-05 10:52:03 +0800

    Patch makeService.sh

--- app-definition/makeService.sh
+++ app-definition/makeService.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 source .env
-export DEVICE_SERVICE_CONFIG=$(jq -c . < device-service.config.json | sed 's/"/\\"/g')
+export DEVICE_SERVICE_CONFIG=$(jq -c . < device-service.config.json.tmpl | sed 's/"/\\\\"/g')
 echo $DEVICE_SERVICE_CONFIG
-sed "s|{{ DEVICE_SERVICE_JSON }}|$DEVICE_SERVICE_CONFIG|g" services/hellosally.service.json > hellosally.service.json
+sed "s|{{ DEVICE_SERVICE_JSON }}|$DEVICE_SERVICE_CONFIG|g" services/hellosally.service.json.tmpl > hellosally.service.json
 sed -i "s|{{ KUIPER_IP }}|$KUIPER_IP|g" hellosally.service.json
 sed -i "s|{{ KUIPER_PORT }}|$KUIPER_PORT|g" hellosally.service.json
 sed -i "s|{{ INFLUXDB_IP }}|$INFLUXDB_IP|g" hellosally.service.json
